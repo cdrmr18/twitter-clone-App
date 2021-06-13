4de4d6561be3874cc4f03d9b8acf5abf511c9f2f
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-13 15:16:07 +0200

    worked on displaying user info but could not access data so updated html

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ const onEnter = (e) => {
 const getTwitterData = () => {
     const query = document.getElementById('user-search-input').value;
     const encodedQuery = encodeURIComponent(query);
-    const url = `${URL}?query=${encodedQuery}&max_results=10&lang=en&tweet.fields=text,attachments&expansions=attachments.media_keys&media.fields=url,preview_image_url&user.fields=name,username`;
+    const url = `${URL}?query=${encodedQuery}&max_results=10&lang=en`;
 
     if (!query) return;
     fetch(url)
@@ -27,24 +27,6 @@ const buildTweets = (tweets) => {
     tweets.map((tweet) => {
         twitterContent += `
             <div class="tweet-container">
-                        <div class="tweet-user-info">
-                            <div class="tweet-user-avatar">
-                                <img src="https://images.unsplash.com/photo-1542513217-0b0eedf7005d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt="avatar" class="avatar">
-                            </div>
-                            <div class="tweet-user-name-container">
-                                <div class="tweet-user-fullname">
-                                    Yonce Knowledge
-                                </div>
-                                <div class="tweet-user-username">
-                                    @yonce
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="tweet-images-container">
-                            <img src="https://images.unsplash.com/photo-1593878024377-b38927fc7689?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmxhY2slMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=60" alt="tweet image">
-                        </div>
-
                         <div class="tweet-text-container">
                                 ${tweet.text}
                         </div>
